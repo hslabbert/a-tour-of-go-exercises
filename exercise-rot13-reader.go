f09c86e377f79e94253d4a5777cd3bd038311a13
Hugo Slabbert <hugo@slabnet.com>
2021-02-09 16:06:01 +0000

    simplify rot13 decoder with +/-13 depending on index

--- exercise-rot13-reader.go
+++ exercise-rot13-reader.go
@@ -13,17 +13,11 @@ type rot13Reader struct {
 func (r rot13Reader) Read(b []byte) (int, error) {
 	for {
 		n, err := r.r.Read(b)
-		for i := 0; i < 20; i++ {
-			if 65 < int(b[i]) && int(b[i]) < 90 {
+		for i := 0; i < n; i++ {
+			if (int(b[i]) >= 65 && int(b[i]) <= 77) || (int(b[i]) >= 97 && int(b[i]) <= 109) {
 				b[i] = b[i] + byte(13)
-				if int(b[i]) > 90 {
-					b[i] = b[i] - byte(26)
-				}
-			} else if 97 < int(b[i]) && int(b[i]) < 122 {
-				b[i] = b[i] + byte(13)
-				if int(b[i]) > 122 {
-					b[i] = b[i] - byte(26)
-				}
+			} else if (int(b[i]) >= 78 && int(b[i]) <= 90) || (int(b[i]) >= 110 && int(b[i]) <= 122) {
+				b[i] = b[i] - byte(13)
 			}
 		}
 		return n, err
